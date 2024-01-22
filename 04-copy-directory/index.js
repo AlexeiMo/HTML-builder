b007e1b248c85e030dec697e641f59f1f017224d
AlexeiMo <47743960+RuslanGitelman@users.noreply.github.com>
2024-01-22 14:52:46 +0300

    refactor: modify directory copy function to accomodate task 6 needs

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,20 +3,28 @@ const path = require('path');
 
 const dirPath = path.join(__dirname, 'files');
 const dirCopyPath = path.join(__dirname, 'files-copy');
-fs.rmdir(dirCopyPath, { recursive: true }, () => {
-  fs.mkdir(dirCopyPath, { recursive: true }, () => {
-    fs.readdir(
-      dirPath,
-      { withFileTypes: true, encoding: 'utf-8' },
-      (_err, files) => {
-        files.forEach((file) => {
-          const filePath = path.join(dirPath, file.name);
-          const fileCopyPath = path.join(dirCopyPath, file.name);
-          fs.copyFile(filePath, fileCopyPath, (_err) => {
-            if (_err) process.stdout.write(_err.message);
+
+const copyDir = (dirPath, copyDirPath) => {
+  fs.rmdir(copyDirPath, { recursive: true }, () => {
+    fs.mkdir(copyDirPath, { recursive: true }, () => {
+      fs.readdir(
+        dirPath,
+        { withFileTypes: true, encoding: 'utf-8' },
+        (_err, files) => {
+          files.forEach((file) => {
+            const filePath = path.join(dirPath, file.name);
+            const fileCopyPath = path.join(copyDirPath, file.name);
+            if (file.isDirectory()) copyDir(filePath, fileCopyPath);
+            else {
+              fs.copyFile(filePath, fileCopyPath, (_err) => {
+                if (_err) process.stdout.write(_err.message + '\n');
+              });
+            }
           });
-        });
-      },
-    );
+        },
+      );
+    });
   });
-});
+};
+
+copyDir(dirPath, dirCopyPath);
