26960cc56d2754a11703491922aeb5a203c21aa4
AlexeiMo <47743960+RuslanGitelman@users.noreply.github.com>
2024-01-22 11:21:50 +0300

    feat: complete third task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const dirPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(
+  dirPath,
+  { withFileTypes: true, encoding: 'utf-8' },
+  (_error, files) => {
+    files.forEach((file) => {
+      if (!file.isDirectory()) {
+        const fileName = path.basename(file.name).split('.')[0];
+        const extName = path.extname(file.name).slice(1);
+
+        const filePath = path.join(dirPath, file.name);
+        fs.stat(filePath, (_error, stats) => {
+          const fileSize = stats.size;
+          process.stdout.write(
+            fileName + ' - ' + extName + ' - ' + fileSize + 'b\n',
+          );
+        });
+      }
+    });
+  },
+);
