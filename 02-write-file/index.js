bb461ed54c540b3ccd573d9fda45626b75f42913
AlexeiMo <prestigio458@gmail.com>
2024-01-23 13:49:45 +0300

    fix: modify read logic to avoid trimming of postfix whitespaces

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -14,7 +14,7 @@ function exitProgram() {
 stdout.write('Hello! Please, enter the text you want to put in the file:\n');
 
 stdin.on('data', (data) => {
-  data = data.toString().trimEnd();
+  data = data.toString().slice(0, -2);
   if (data === 'exit') exitProgram();
   writeableStream.write(data);
 });
