16b13bdeb377203443b0bbf78c364bbda4c01b2b
AlexeiMo <prestigio458@gmail.com>
2024-01-23 13:52:22 +0300

    fix: modify read file name logic to include all name instead of first part

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -9,14 +9,18 @@ fs.readdir(
   (_error, files) => {
     files.forEach((file) => {
       if (!file.isDirectory()) {
-        const fileName = path.basename(file.name).split('.')[0];
+        const fileName = path
+          .basename(file.name)
+          .split('.')
+          .slice(0, -1)
+          .join('.');
         const extName = path.extname(file.name).slice(1);
 
         const filePath = path.join(dirPath, file.name);
         fs.stat(filePath, (_error, stats) => {
           const fileSize = stats.size;
           process.stdout.write(
-            fileName + ' - ' + extName + ' - ' + fileSize + 'b\n',
+            fileName + ' - ' + extName + ' - ' + fileSize + ' b\n',
           );
         });
       }
